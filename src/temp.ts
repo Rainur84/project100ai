676922d52bf7539443d85e87a8f29f3d43f4575f
Rainur <rainur.sh@gmail.com>
2025-06-03 21:28:43 +0500

    chore: manual rebuild to update news

--- src/temp.ts
+++ src/temp.ts
@@ -1,2 +1,1 @@
-// touch rebuild
-// trigger rebuild Tue Jun  3 21:20:45 +05 2025
+// trigger rebuild
--- update-news.sh
+++ update-news.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-echo "// trigger rebuild $(date)" >> src/temp.ts
+echo "// trigger rebuild" > src/temp.ts
 git add .
-git commit -m "chore: trigger rebuild for news update"
+git commit -m "chore: manual rebuild to update news"
 git push
